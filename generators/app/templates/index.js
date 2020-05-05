18b35775881be86d829df303835a685f87f24eb0
Bala <sbalasubramanian96@gmail.com>
2020-05-05 11:09:32 +0530

    fix: added cors

--- generators/app/templates/index.js
+++ generators/app/templates/index.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const path = require("path");
 const logger = require("./middleware/logger");
+const cors = require("cors");
 
 const app = express();
 
 // Init middleware
 app.use(logger);
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
